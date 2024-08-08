958a93b49fe0df63f6e43fdca8e31f11cf430ee4
promovi2 <112362935+promovi2@users.noreply.github.com>
2024-08-08 19:14:27 -0300

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,6 +1,12 @@
+# rxconfig.py
+
+import os
 import reflex as rx
 
+# Obtén el puerto desde la variable de entorno PORT (por defecto 10000 si no está configurado)
+port = int(os.environ.get('PORT', 10000))
+
 config = rx.Config(
-    app_name="reflex_web",
-    api_url="https://reflex-web-v370.onrender.com:8000",
+    app_name="your_app_name",  # Cambia esto al nombre de tu aplicación
+    api_url=f"http://localhost:{port}",  # Asegúrate de que el backend esté configurado para escuchar en este puerto
 )
